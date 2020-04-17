7734b17568c4d2ab36bad696f76cd90282e8e641
Marcelo Amorim <marcelo.brasil.amorim@gmail.com>
2020-04-16 22:22:08 -0300

    implements form submit and remove repositories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => setRepos(response.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const result = await api.post('/repositories', {
+      id: "123",
+      url: "https://github.com/josepholiveira",
+      title: "Desafio ReactJS",
+      techs: ["React", "Node.js"],
+    });
+    
+    const repo = result.data;
+    setRepos([...repos, repo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const result = await api.delete(`/repositories/${id}`);
+
+    if (result.status === 204){
+      setRepos(repos.filter(repo => repo.id !== id));
+    }
+    
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repos.map((repo) => {
+          return (
+            <li key={repo.id}>
+              {repo.title}
+              <button onClick={() => handleRemoveRepository(repo.id)}>Remover</button>
+            </li>
+          );
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
